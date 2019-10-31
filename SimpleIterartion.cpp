ed232b9f048446532ffac7104904fa7cacea3ff6
KartoonYoko <crashed9001@gmail.com>
2019-10-31 16:26:47 +0900

    Release SimpleItaration

--- SimpleIterartion.cpp
+++ SimpleIterartion.cpp
@@ -1,7 +1,4 @@
-﻿// SimpleIterartion.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
-
-#include <iostream>
+﻿#include <iostream>
 #include <vector>
 #include <math.h>
 #include <iomanip>
@@ -28,6 +25,7 @@ void outm(vector<vector<double>> matrix, int n) {
 	cout.unsetf(ios::fixed);
 }
 
+//сравнивает два вектора. Size - размер векторов. Accuracy - допустимая точность для совпадения векторов.
 bool CompareTwoVectors(const vector<double>& vec1, const vector<double>& vec2, const int& size, const double& accuracy) {
 	bool answer = false;
 	for (int i = 0; i < size; i++) {
@@ -42,6 +40,8 @@ bool CompareTwoVectors(const vector<double>& vec1, const vector<double>& vec2, c
 	}
 	return answer;
 }
+
+
 
 vector<double> MethodOfIteration(const vector<vector<double>>& matrix, const int& n) {
 	vector<double> recAns(n, 0);
@@ -58,16 +58,60 @@ vector<double> MethodOfIteration(const vector<vector<double>>& matrix, const int
 	do {
 		prevAns = recAns;
 		for (int i = 0; i < n; i++) {
+			recAns[i] = 0;
+		}
+		for (int i = 0; i < n; i++) {
 			for (int j = 0; j < n; j++) {
-				double sum = 0;
 				if (i != j) {
+					double h1 = prevAns[j];
+					double h2 = bufMat[i][j];
 					recAns[i] += prevAns[j] * bufMat[i][j];
+					double h3 = recAns[i];
+					int ar = 0;
+				}
+				else {
+					recAns[i] += bufMat[i][j];
+					double h1 = recAns[i];
+					double h2 = bufMat[i][j];
+					int ar = 0;
+				}
+			}
+		}
+	} while (!CompareTwoVectors(recAns, prevAns, n, 0.001));
+	return recAns;
+}
+
+
+
+vector<double> MethodOfZeidel(const vector<vector<double>>& matrix, const int& n) {
+	vector<double> recAns(n, 0);
+	vector<double> prevAns(n, 0);
+	vector <vector<double>> bufMat(n, vector<double>(n));
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			if (i != j)
+				bufMat[i][j] = -matrix[i][j] / matrix[i][i];
+			else
+				bufMat[i][j] = matrix[i][n] / matrix[i][i];
+		}
+	}
+	do {
+		prevAns = recAns;
+		for (int i = 0; i < n; i++) {
+			recAns[i] = 0;
+		}
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				if (i != j) {
+					if (i > j) recAns[i] += recAns[j] * bufMat[i][j];
+					else recAns[i] += prevAns[j] * bufMat[i][j];
 				}
 				else {
 					recAns[i] += bufMat[i][j];
 				}
 			}
 		}
+		
 	} while (CompareTwoVectors(recAns, prevAns, n, 0.001));
 	return recAns;
 }
@@ -75,46 +119,60 @@ vector<double> MethodOfIteration(const vector<vector<double>>& matrix, const int
 
 int main()
 {
-	matrix[0][1] = -0.66;
-	matrix[1][1] = -0.59;
-	matrix[2][1] = -0.8;
-	matrix[3][1] = -0.97;
-	matrix[4][1] = 0.33;
-	matrix[0][2] = 0.46;
-	matrix[1][2] = -0.46;
-	matrix[2][2] = -0.24;
-	matrix[3][2] = -0.46;
-	matrix[4][2] = -0.28;
-	matrix[0][3] = 0.62;
-	matrix[1][3] = 0.63;
-	matrix[2][3] = -0.51;
-	matrix[3][3] = 0.27;
-	matrix[4][3] = -0.94;
-	matrix[0][4] = 0.83;
-	matrix[1][4] = -0.68;
-	matrix[2][4] = 0.98;
-	matrix[3][4] = -0.45;
-	matrix[4][4] = -0.92;
-	matrix[0][0] = -0.02;
-	matrix[1][0] = 0.52;
-	matrix[2][0] = 0.28;
-	matrix[3][0] = 0.38;
-	matrix[4][0] = 0.17;
 
-	matrix[0][5] = 0.24;
-	matrix[1][5] = -0.98;
-	matrix[2][5] = -0.94;
-	matrix[3][5] = -0.84;
-	matrix[4][5] = -0.79;
+	const int n = 5;
+	vector <vector<double>> matrix(n, vector<double>(n + 1, 1));
+	vector <vector<double>> nevyaz(n, vector<double>(n + 1, 1));
+	vector<double> answer(n);
+	double buf, buf2;
+	double eps = 1 * pow(10, -2);
+
+	matrix[0][1] = 6;
+	matrix[1][1] = 25;
+	matrix[2][1] = -1;
+	matrix[3][1] = 5;
+	matrix[4][1] = 8;
+	matrix[0][2] = 5;
+	matrix[1][2] = -9;
+	matrix[2][2] = 24;
+	matrix[3][2] = -6;
+	matrix[4][2] = -7;
+	matrix[0][3] = 1;
+	matrix[1][3] = -6;
+	matrix[2][3] = 4;
+	matrix[3][3] = 33;
+	matrix[4][3] = 8;
+	matrix[0][4] = -2;
+	matrix[1][4] = 0;
+	matrix[2][4] = -9;
+	matrix[3][4] = 9;
+	matrix[4][4] = 38;
+	matrix[0][0] = 25;
+	matrix[1][0] = 3;
+	matrix[2][0] = -2;
+	matrix[3][0] = 2;
+	matrix[4][0] = -5;
+				   
+	matrix[0][5] = 35;
+	matrix[1][5] = 26;
+	matrix[2][5] = 48;
+	matrix[3][5] = 86;
+	matrix[4][5] = 42;
+
+	outm(matrix, n);
+	answer = MethodOfIteration(matrix, n);
+
+	cout << endl << answer[0] << " " << answer[1] << " " << answer[2] << " " << answer[3] << " " << answer[4] << endl;
+	nevyaz = matrix;
+	for (int i = 0; i < n; i++) {
+		buf = 0;
+		for (int j = 0; j < n; j++) {
+			buf += nevyaz[i][j] * answer[j];
+		}
+		if (abs(nevyaz[i][n] - buf) > eps) cout << "Not required accuracy in line " << i << endl;
+		else cout << "There is required accuracy in line " << i << endl;
+	}
+
+
+
 }
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
